2438f7715c9a4431304afd57d767804244d3003b
turki-aloufi <turkialoufi44@gmail.com>
2024-09-28 03:50:38 +0300

    finish home page app bar ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project8/screens/auth_screens/login_screen.dart';
+import 'package:project8/screens/user_screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginScreen()
+      home: HomeScreen()
     );
   }
 }
--- lib/screens/user_screens/home_screen.dart
+++ lib/screens/user_screens/home_screen.dart
@@ -5,6 +5,54 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xffEEEDEA),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 53,
+                      child: Image.asset(
+                        "assets/images/default_profile_img.png",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome Coffee Addict",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text("Sara",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold))
+                    ],
+                  ),
+                ],
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.notifications_none_outlined,
+                    size: 30,
+                  ))
+            ],
+          ),
+        ],
+      )),
+    );
   }
 }
