e66b8e8926204e51ecd14ec84dd2cdb10a2a7792
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-09-29 21:56:50 +0300

    favorites card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project8/screens/auth_screens/login_screen.dart';
+import 'package:project8/screens/user_screens/user_navigation.dart';
 // import 'package:project8/screens/user_screens/user_navigation.dart';
 import 'package:project8/services/setup.dart';
 
@@ -15,7 +16,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginScreen()
+      home: UserNavigation()
     );
   }
 }
