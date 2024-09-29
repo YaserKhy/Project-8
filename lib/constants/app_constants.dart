a4c07bad3a2f816dbb6890639c61abb54a564823
Rahafahg <irahaf.88@gmail.com>
2024-09-29 21:51:36 +0300

    order tapbar and card

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,17 +3,19 @@ import 'package:flutter/material.dart';
 class AppConstants {
   // colors
   static Color authBgColor = const Color(0xff4C6A7C).withOpacity(.8);
-  
+
   static const Color mainBgColor = Color(0xffEEEDEA);
   static const Color mainRed = Color(0xff984A40);
   static const Color mainWhite = Color(0xffE6E4DF);
   static const Color mainBlue = Color(0xff4C6A7C);
-  
+  static const Color mainlightBlue = Color(0xff93B0C4);
+
   static const Color textColor = Color(0xff313131);
   static const Color subTextColor = Color(0xff525252);
 
   static const Color unselectedColor = Color(0xffA8A8A8);
-  
+
   // regex
-  static RegExp emailRegex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+  static RegExp emailRegex =
+      RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
 }
