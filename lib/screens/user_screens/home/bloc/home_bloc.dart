e2e6771d641dc3f1ce4aceeca09eca5742703d5c
turki-aloufi <turkialoufi44@gmail.com>
2024-10-01 00:21:31 +0300

    manage favorite icon state

--- lib/screens/user_screens/home/bloc/home_bloc.dart
+++ lib/screens/user_screens/home/bloc/home_bloc.dart
@@ -1,24 +1,37 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:meta/meta.dart';
+import 'package:project8/data_layers/auth_layer.dart';
+import 'package:project8/data_layers/item_layer.dart';
+import 'package:project8/data_layers/supabase_layer.dart';
 import 'package:project8/helpers/db_functions.dart';
+import 'package:project8/models/item_model.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  List<ItemModel> favorites = [];
   HomeBloc() : super(HomeInitial()) {
     on<GetAllItemsEvent>(getItems);
     on<ChangeCategoryEvent>(changeCategory);
+    on<ToggleFavoriteEvent>(toggleFavorite);
   }
 
   FutureOr<void> getItems(
       GetAllItemsEvent event, Emitter<HomeState> emit) async {
     try {
       emit(LoadingState());
+      // Fetch items and assign them to the allItems list
       await getAllItems();
-      emit(SuccessState(selectedCategory: "All"));
+      GetIt.I.get<ItemLayer>().items; // Assuming this returns a list of items
+      emit(SuccessState(
+          selectedCategory: "All",
+          items: GetIt.I.get<ItemLayer>().items,
+          favorites: favorites));
+      print(GetIt.I.get<ItemLayer>().items.length);
     } catch (error) {
       emit(ErrorState(msg: error.toString()));
     }
@@ -26,6 +39,35 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
 
   FutureOr<void> changeCategory(
       ChangeCategoryEvent event, Emitter<HomeState> emit) {
-    emit(SuccessState(selectedCategory: event.category));
+    if (state is SuccessState) {
+      final successState = state as SuccessState;
+      emit(SuccessState(
+        selectedCategory: event.category,
+        items: successState.items,
+        favorites: favorites,
+      ));
+    }
+  }
+
+  Future<void> toggleFavorite(
+      ToggleFavoriteEvent event, Emitter<HomeState> emit) async {
+    if (favorites.contains(event.item)) {
+      
+      favorites.remove(event.item);
+    } else {
+      await GetIt.I.get<SupabaseLayer>().supabase.rpc('fav_item', params: {
+        'item_id': event.item.itemId,
+        'customer_id': GetIt.I.get<AuthLayer>().customer?.id
+      });
+      favorites.add(event.item);
+    }
+    if (state is SuccessState) {
+      final successState = state as SuccessState;
+      emit(SuccessState(
+        selectedCategory: successState.selectedCategory,
+        items: successState.items,
+        favorites: favorites,
+      ));
+    }
   }
 }
--- lib/widgets/cards/item_card.dart
+++ lib/widgets/cards/item_card.dart
@@ -1,12 +1,82 @@
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:project8/constants/app_constants.dart';
+// import 'package:project8/models/item_model.dart';
+
+// class ItemCard extends StatelessWidget {
+//   final ItemModel item;
+//   final Function()? onFav;
+//   const ItemCard({super.key,required this.item, this.onFav});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 211,
+//       width: 175,
+//       decoration: BoxDecoration(
+//           color: const Color(0xffF7F6F4),
+//           borderRadius: BorderRadius.circular(20)),
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Container(
+//               height: 100,
+//               width: 175,
+//               decoration: BoxDecoration(
+//                   color: const Color.fromARGB(255, 232, 231, 231),
+//                   borderRadius: BorderRadius.circular(15)),
+//               child: Image.network(item.image),
+//             ),
+//             Text(
+//               item.name,
+//               style: const TextStyle(fontSize: 18),
+//             ),
+//             Text("${item.price} SR", style: const TextStyle(fontSize: 12)),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Row(
+//                   children: [
+//                     const FaIcon(
+//                       FontAwesomeIcons.fireFlameCurved,
+//                       size: 15,
+//                       color: AppConstants.mainRed,
+//                     ),
+//                     const SizedBox(
+//                       width: 5,
+//                     ),
+//                     Text("${item.calories} Cal"),
+//                   ],
+//                 ),
+//                 IconButton(
+//                     onPressed: onFav,
+//                     icon: const Icon(Icons.favorite, color: AppConstants.mainRed,))
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// item_card.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:project8/constants/app_constants.dart';
 import 'package:project8/models/item_model.dart';
+import 'package:project8/screens/user_screens/home/bloc/home_bloc.dart';
 
 class ItemCard extends StatelessWidget {
   final ItemModel item;
-  final Function()? onFav;
-  const ItemCard({super.key,required this.item, this.onFav});
+  const ItemCard({
+    super.key,
+    required this.item,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +84,9 @@ class ItemCard extends StatelessWidget {
       height: 211,
       width: 175,
       decoration: BoxDecoration(
-          color: const Color(0xffF7F6F4),
-          borderRadius: BorderRadius.circular(20)),
+        color: const Color(0xffF7F6F4),
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -25,8 +96,9 @@ class ItemCard extends StatelessWidget {
               height: 100,
               width: 175,
               decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 232, 231, 231),
-                  borderRadius: BorderRadius.circular(15)),
+                color: const Color.fromARGB(255, 232, 231, 231),
+                borderRadius: BorderRadius.circular(15),
+              ),
               child: Image.network(item.image),
             ),
             Text(
@@ -44,17 +116,30 @@ class ItemCard extends StatelessWidget {
                       size: 15,
                       color: AppConstants.mainRed,
                     ),
-                    const SizedBox(
-                      width: 5,
-                    ),
+                    const SizedBox(width: 5),
                     Text("${item.calories} Cal"),
                   ],
                 ),
-                IconButton(
-                    onPressed: onFav,
-                    icon: const Icon(Icons.favorite, color: AppConstants.mainRed,))
+                BlocBuilder<HomeBloc, HomeState>(
+                  builder: (context, state) {
+                    if (state is SuccessState) {
+                      final isFavorite = state.favorites.contains(item);
+
+                      return IconButton(
+                        icon: Icon(
+                          isFavorite ? Icons.favorite : Icons.favorite_border,
+                          color: isFavorite ? AppConstants.mainRed : null,
+                        ),
+                        onPressed: () {
+                          context.read<HomeBloc>().add(ToggleFavoriteEvent(item: item));
+                        },
+                      );
+                    }
+                    return const SizedBox.shrink(); // Default fallback
+                  },
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
