53301a45d0e41982c163ab7ba53b4ebf86d68706
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-10-01 21:23:11 +0300

    view item from fav screen

--- lib/screens/user_screens/Favorite/favorite_screen.dart
+++ lib/screens/user_screens/Favorite/favorite_screen.dart
@@ -4,9 +4,12 @@ import 'package:get_it/get_it.dart';
 import 'package:lottie/lottie.dart';
 import 'package:project8/constants/app_constants.dart';
 import 'package:project8/data_layers/item_layer.dart';
+import 'package:project8/extensions/screen_nav.dart';
 import 'package:project8/extensions/screen_size.dart';
 import 'package:project8/models/item_model.dart';
 import 'package:project8/screens/user_screens/Favorite/bloc/favorite_bloc.dart';
+import 'package:project8/screens/user_screens/home/bloc/home_bloc.dart' as home_bloc;
+import 'package:project8/screens/user_screens/view_item.dart';
 import 'package:project8/widgets/cards/favorites_item_card.dart';
 import 'package:project8/widgets/other/page_title.dart';
 
@@ -15,56 +18,74 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppConstants.mainBgColor,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const PageTitle(title: "Favorites"),
-              const SizedBox(height: 30),
-              BlocBuilder<FavoriteBloc, FavoriteState>(
-                builder: (context, state) {
-                  if (state is LoadingState) {
-                    return SizedBox(
-                      height: context.getHeight(divideBy: 1.5),
-                      child: Center(child: LottieBuilder.asset("assets/images/Animation - 1727608827461.json",))
-                    );
-                  }
-                  if (state is ErrorState) {
-                    return SizedBox(
-                      height: context.getHeight(divideBy: 1.5),
-                      child: Center(child: Text(state.msg))
-                    );
-                  }
-                  if (state is SuccessState) {
-                    final List<ItemModel> fav = GetIt.I.get<ItemLayer>().favItems;
-                    if(fav.isEmpty) {
-                      return SizedBox(
-                        height: context.getHeight(divideBy: 1.5),
-                        child: const Center(child: Text("No Items Added Yet"))
-                      );
-                    }
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: ListView.separated(
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: fav.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index)=> FavoritesCard(
-                          favItem: fav[index]
-                        ),
-                        separatorBuilder: (context, index) => const SizedBox(height: 20),
-                      ),
-                    );
-                  }
-                  return const SizedBox.shrink();
-                },
+    final bloc = context.read<FavoriteBloc>();
+    return BlocProvider(
+      create: (context) => home_bloc.HomeBloc(),
+      child: Builder(
+        builder: (context) {
+          final homeBloc = context.read<home_bloc.HomeBloc>();
+          return Scaffold(
+            backgroundColor: AppConstants.mainBgColor,
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const PageTitle(title: "Favorites"),
+                    const SizedBox(height: 30),
+                    BlocBuilder<FavoriteBloc, FavoriteState>(
+                      builder: (context, state) {
+                        if (state is LoadingState) {
+                          return SizedBox(
+                            height: context.getHeight(divideBy: 1.5),
+                            child: Center(child: LottieBuilder.asset("assets/images/Animation - 1727608827461.json"))
+                          );
+                        }
+                        if (state is ErrorState) {
+                          return SizedBox(
+                            height: context.getHeight(divideBy: 1.5),
+                            child: Center(child: Text(state.msg))
+                          );
+                        }
+                        if (state is SuccessState) {
+                          final List<ItemModel> fav = GetIt.I.get<ItemLayer>().favItems;
+                          if (fav.isEmpty) {
+                            return SizedBox(
+                              height: context.getHeight(divideBy: 1.5),
+                              child: const Center(child: Text("No Items Added Yet"))
+                            );
+                          }
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: ListView.separated(
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: fav.length,
+                              shrinkWrap: true,
+                              itemBuilder: (context, index) => FavoritesCard(
+                                favItem: fav[index],
+                                onView: () => context.push(
+                                  screen: ViewItem(item: fav[index]),
+                                  updateInfo: (p0) {
+                                    if (p0 != null) {
+                                      homeBloc.add(home_bloc.GetAllItemsEvent());
+                                      bloc.add(GetFavItemsEvent());
+                                    }
+                                  }
+                                ),
+                              ),
+                              separatorBuilder: (context, index) => const SizedBox(height: 20),
+                            ),
+                          );
+                        }
+                        return const SizedBox.shrink();
+                      },
+                    ),
+                    const SizedBox(height: 70),
+                  ],
+                ),
               ),
-              const SizedBox(height: 70),
-            ],
-          ),
-        ),
+            ),
+          );
+        }
       ),
     );
   }
--- lib/widgets/cards/favorites_item_card.dart
+++ lib/widgets/cards/favorites_item_card.dart
@@ -8,82 +8,87 @@ import 'package:project8/screens/user_screens/Favorite/bloc/favorite_bloc.dart';
 
 class FavoritesCard extends StatelessWidget {
   final ItemModel favItem;
-  const FavoritesCard({super.key, required this.favItem});
+  final Function()? onView;
+  const FavoritesCard({super.key, required this.favItem, this.onView});
 
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<FavoriteBloc>();
-    return Container(
-      height: 135,
-      width: context.getWidth(divideBy: 1.1),
-      decoration: BoxDecoration(
-        color: const Color(0xffF7F6F4),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: const Color.fromARGB(255, 191, 191, 191).withOpacity(0.8), // Shadow color
-            spreadRadius: 3, // Spread of the shadow
-            blurRadius: 5, // Softness of the shadow
-            offset: const Offset(6, 6), // Horizontal and vertical offset
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Container(
-              height: 100,
-              width: context.getWidth(divideBy: 3),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 232, 231, 231),
-                borderRadius: BorderRadius.circular(15)
-              ),
-              child: Image.network(favItem.image),
+    return InkWell(
+      overlayColor: WidgetStateColor.transparent,
+      onTap: onView,
+      child: Container(
+        height: 135,
+        width: context.getWidth(divideBy: 1.1),
+        decoration: BoxDecoration(
+          color: const Color(0xffF7F6F4),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(255, 191, 191, 191).withOpacity(0.8), // Shadow color
+              spreadRadius: 3, // Spread of the shadow
+              blurRadius: 5, // Softness of the shadow
+              offset: const Offset(6, 6), // Horizontal and vertical offset
             ),
-            const SizedBox(width: 20,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 10,),
-                Text(
-                  favItem.name,
-                  style: const TextStyle(fontSize: 22, fontFamily: "Average"),
-                ),
-                Text(
-                  "${favItem.price} SR",
-                  style:const TextStyle(fontSize: 16, fontFamily: "Average")
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 70,
-                      child: Row(
-                        children: [
-                          const FaIcon(FontAwesomeIcons.fireFlameCurved,size: 15,color: AppConstants.mainRed,),
-                          const SizedBox(width: 5,),
-                          Text(
-                            "${favItem.calories} Cal",
-                            style: const TextStyle(fontFamily: "Average")
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 80,),
-                    BlocBuilder<FavoriteBloc, FavoriteState>(
-                      builder: (context, state) {
-                        return IconButton(
-                          onPressed: () => bloc.add(ToggleFavoriteEvent(item: favItem)),
-                          icon: const Icon(Icons.favorite,color: AppConstants.mainRed)
-                        );
-                      },
-                    )
-                  ],
-                )
-              ],
-            )
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Container(
+                height: 100,
+                width: context.getWidth(divideBy: 3),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 232, 231, 231),
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: Image.network(favItem.image),
+              ),
+              const SizedBox(width: 20,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10,),
+                  Text(
+                    favItem.name,
+                    style: const TextStyle(fontSize: 22, fontFamily: "Average"),
+                  ),
+                  Text(
+                    "${favItem.price} SR",
+                    style:const TextStyle(fontSize: 16, fontFamily: "Average")
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 70,
+                        child: Row(
+                          children: [
+                            const FaIcon(FontAwesomeIcons.fireFlameCurved,size: 15,color: AppConstants.mainRed,),
+                            const SizedBox(width: 5,),
+                            Text(
+                              "${favItem.calories} Cal",
+                              style: const TextStyle(fontFamily: "Average")
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 80,),
+                      BlocBuilder<FavoriteBloc, FavoriteState>(
+                        builder: (context, state) {
+                          return IconButton(
+                            onPressed: () => bloc.add(ToggleFavoriteEvent(item: favItem)),
+                            icon: const Icon(Icons.favorite,color: AppConstants.mainRed)
+                          );
+                        },
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
